055131f69a55850f18ec917c1640f262c2cec89d
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2015-05-19 08:26:25 +0530

    [feat] Add helper utilites to server.sync.SyncExchange.
    Baby steps towards integrating the ZeroMQ transport for sync into soledad.

--- server/src/leap/soledad/server/sync.py
+++ server/src/leap/soledad/server/sync.py
@@ -225,6 +225,26 @@ class SyncExchange(sync.SyncExchange):
             self._db, self.source_replica_uid, sync_id)
 
 
+    def get_docs(self, changed_doc_ids, check_for_conflicts=True,
+                 include_deleted=False):
+        """
+        Helper utility to provide access to private instance
+        variable self._db.
+        """
+        return self._db.get_docs(changed_doc_ids, check_for_conflicts,
+                                 include_deleted)
+
+    def get_generation_info(self):
+        """
+        Wrapper to access private method _get_generation_info of private
+        attribute _db.
+
+        :return: A tuple containing the docs by generation, the updated
+                 target generation and the transaction id.
+        :rtype: tuple
+        """
+        return self._db._get_generation_info()
+
     def find_changes_to_return(self, received):
         """
         Find changes to return.
